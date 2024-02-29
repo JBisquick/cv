ebc5afe959a7d22530818d5a33c55793d88c254c
JBisquick <felipebirch@gmail.com>
2024-02-28 16:15:27 -0800

    Add cancel/add button to education experience form

--- src/components/EducationInfo.jsx
+++ src/components/EducationInfo.jsx
@@ -1,40 +1,66 @@
-function EducationInfo({onSubmit, education}) {
+import { useState } from 'react';
+
+function EducationInfo({ onSubmit, education }) {
+  const [formState, setFormState] = useState('hide');
+
+  function hide(e) {
+    e.preventDefault();
+    setFormState('hide');
+  }
+
+  function show() {
+    setFormState('show');
+  }
+
   return (
     <div>
       <h1>Educational Experience</h1>
-      <form onSubmit={onSubmit}>
-        <div>
-          <label htmlFor="degree">Degree</label>
-          <input type="text" id="degree" name="degree" />
-        </div>
-        <div>
-          <label htmlFor="school">School</label>
-          <input type="text" id="school" name="school" />
-        </div>
-        <div>
-          <label htmlFor="location">City, Country</label>
-          <input type="text" id="location" name="location" />
-        </div>
-        <div>
-          <div>
-            <label htmlFor="start-date">Start Date</label>
-            <input type="date" id="start-date" name="startDate" />
-          </div>
-          <div>
-            <label htmlFor="end-date">End Date</label>
-            <input type="date" id="end-date" name="endDate" />
-          </div>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-
+      {formState === 'hide' ? (
+        <button onClick={show}>Add</button>
+      ) : (
+        <>
+          <button onClick={hide}>Cancel</button>
+          <form
+            onSubmit={(e) => {
+              onSubmit(e);
+              hide(e);
+            }}
+          >
+            <div>
+              <label htmlFor="degree">Degree</label>
+              <input type="text" id="degree" name="degree" />
+            </div>
+            <div>
+              <label htmlFor="school">School</label>
+              <input type="text" id="school" name="school" />
+            </div>
+            <div>
+              <label htmlFor="location">City, Country</label>
+              <input type="text" id="location" name="location" />
+            </div>
+            <div>
+              <div>
+                <label htmlFor="start-date">Start Date</label>
+                <input type="date" id="start-date" name="startDate" />
+              </div>
+              <div>
+                <label htmlFor="end-date">End Date</label>
+                <input type="date" id="end-date" name="endDate" />
+              </div>
+            </div>
+            <button type="submit">Submit</button>
+          </form>
+        </>
+      )}
       {education.map((school) => (
         <div key={school.id}>
           <div>
             <div>{school.degree}</div>
             <div>{school.school}</div>
             <div>{school.school}</div>
-            <div>{school.startDate} - {school.endDate}</div>
+            <div>
+              {school.startDate} - {school.endDate}
+            </div>
           </div>
           <button>Edit</button>
         </div>
